92e448ac03017ceeb9e2b6ae38a03b8fc1340180
Sean Robinson <sean.robinson@scottsdalecc.edu>
2018-08-28 08:17:07 -0700

    Begin Spar source code
    Signed-off-by: Sean Robinson <sean.robinson@scottsdalecc.edu>

---
+++ js/spar.js
@@ -0,0 +1,21 @@
+/*
+ * spar jQuery UI Widget
+ *
+ * A multi-round countdown timer to support in-persons exams.
+ *
+ * @updated August 28, 2018
+ * @version Talc
+ *
+ * @author Sean Robinson <sean.robinson@scottsdalecc.edu>
+ * @copyright (c) 2018 Scottsdale Community College
+ */
+
+'use strict';
+
+$.widget('scottsdalecc.spar', {
+    options: {
+    },
+
+    _create: function() {
+    }
+});
