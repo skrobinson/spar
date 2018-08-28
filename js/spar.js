a811e114fea1dddc229b9753c603641d4f611f9f
Sean Robinson <sean.robinson@scottsdalecc.edu>
2018-08-28 09:26:16 -0700

    Add default options for running an exam
    These are the usual settings that will be needed during a mineral exam.
    Other helpful mode would the demonstration mode, but that will be added
    at a later date.
    
    Signed-off-by: Sean Robinson <sean.robinson@scottsdalecc.edu>

--- js/spar.js
+++ js/spar.js
@@ -14,6 +14,21 @@
 
 $.widget('scottsdalecc.spar', {
     options: {
+        interval: 60,  // seconds per round (i.e. one mineral)
+        pause: 2,  // hidden delay between rounds
+        rounds: 25,  // number of countdowns to show
+        sound: true,  // play sounds ending each round
+        // Defaults for rounddown appearance
+        timerOpts: {
+            autostart: false,
+            fillStyle: '#FFFFFF',  // solid white interior
+            fontColor: '#000000',  // black text
+            label: ['second', 'seconds'],
+            radius: 52,
+            smooth: true,
+            strokeStyle: '#000000',  // border color
+            strokeWidth: 5  // border width
+        }
     },
 
     _create: function() {
